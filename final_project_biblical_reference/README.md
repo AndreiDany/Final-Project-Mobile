b7f59315fd162218600d1437e2c96745db2aa664
AndreiDany~ <daniel.andrei@emanuel.ro>
2024-01-04 22:26:34 +0200

    first commit

---
+++ final_project_biblical_reference/README.md
@@ -0,0 +1,3 @@
+# final_project_biblical_reference
+
+A new Flutter project.
---
+++ final_project_biblical_reference/android/app/src/main/kotlin/com/example/final_project_biblical_reference/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_project_biblical_reference
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
